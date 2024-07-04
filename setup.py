f01d1081a38828dc6feecccb1f79a9c5ba52e6b8
eloisd <eloi.seidlitz@gmail.com>
2024-07-04 16:59:07 +0200

    setup.py prepared

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Medical Chatbot",
+    version = "0.0.0",
+    author = "Eloi Seidlitz",
+    author_email = 'eloi.seidlitz@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
